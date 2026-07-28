a6ad976d5d917c620bba7aa2cdbbec525254ed44
Luan Vedovoto <vedovotoluan2@gmail.com>
2026-07-28 19:30:35 -0300

    created project

---
+++ src/main/java/com/library/libraryspringjpa/LibrarySpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.library.libraryspringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySpringJpaApplication.class, args);
+    }
+
+}
